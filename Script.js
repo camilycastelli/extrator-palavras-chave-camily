9025cb4af4ac303804c4651020d4584a568eb3a4
Camily <camilycastellimarcelino@gmail.com>
2025-10-28 10:45:02 -0300

    28/10

--- Script.js
+++ Script.js
@@ -14,6 +14,21 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2){
+        return frequencias [p2] - frequencias [p1];
+    }
+    return ordenadas.slice(0, 10);
+
+
+
+}
+
+
+function contaFrequencias(palavras) {
+
     let frequencias = {};
 
     for (let i of palavras) {
@@ -27,5 +42,11 @@ function processaTexto(texto) {
     }
 
     return palavras;
-}
 
+
+
+
+
+
+
+}
