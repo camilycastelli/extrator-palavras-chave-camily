2556be1aa3b7bfc7ac257d5fde5e79dc6872f579
Camily <camilycastellimarcelino@gmail.com>
2025-10-28 09:41:30 -0300

    28/10

--- Script.js
+++ Script.js
@@ -1,12 +1,9 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
-
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
-
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavrasChave = processaTexto(texto);
-
     campoResultado.textContent = palavrasChave.join(", ")
 
 }
@@ -15,5 +12,4 @@ function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
     return palavras;
-    
 }
