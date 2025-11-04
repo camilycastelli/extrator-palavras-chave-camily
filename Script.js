0b6f4a13e29adbebbc3abd2f207540e4a9d6766a
Camily <camilycastellimarcelino@gmail.com>
2025-11-04 10:21:42 -0300

    04/11

--- Script.js
+++ Script.js
@@ -51,16 +51,16 @@ function contaFrequencias(palavras) {
 
 }
 
-function tiraPalavrasRuins(palavras){
-const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
-const palavrasBoas = [];
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
+    const palavrasBoas = [];
 
 
-for (let palavra of palavras){
-    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2){
-        palavrasBoas.push(palavra);
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
     }
-}
-return palavrasBoas;
+    return palavrasBoas;
 
 }
